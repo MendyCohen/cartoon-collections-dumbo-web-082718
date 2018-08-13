53eacf3cdc182cb337d53ef44e4954ef7d0b4e97
Mendy <mendybeltcohen@gmail.com>
2018-08-13 23:12:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(no_more_than_4)
-  no_more_than_4.find do |x|
+  no_more_than_4.any? do |x|
       x.length > 4
        return true
     if x.length < 4
