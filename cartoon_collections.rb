ddd08079a21e5014e5e3b0231f06f8fefec36eda
Mendy <mendybeltcohen@gmail.com>
2018-08-13 22:23:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,9 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(no_more_than_4)
-  
+  no_more_than_4.find do |x|
+    x.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
