f0f57695949e24216c2d18cf337987f8a030cffc
Mendy <mendybeltcohen@gmail.com>
2018-08-13 22:21:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf)
+ count = 1
+ dwarf.collect do |x|
+  puts "#{count}. #{x}"
+   count+=1
+ end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |x|
+    x.capitalize << '!'
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls
   # Your code here
 end
 
