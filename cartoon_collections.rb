008501e3837abad925365795670da7120f45f6ff
Mendy <mendybeltcohen@gmail.com>
2018-08-13 22:51:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,11 @@ end
 
 def long_planeteer_calls(no_more_than_4)
   no_more_than_4.select do |x|
-    if x.length > 4
-        true
-     else
-        false
+     x.length > 4
+       return true
+     if
+        x > 4
+        return false
      end
   end
 end
